c5dcf25f2bf46caa86cc3220d0127c92fb524111
narendrapanchal <narendrapanchal020@gmail.com>
2024-12-01 13:35:07 +0530

    As

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -43,3 +43,18 @@ function AboutCard() {
 }
 
 export default AboutCard;
+var moveZeroes = function(nums) {
+  for(let i=0;i<nums.length;i++){
+      if(nums[i]==0){
+
+      for(let j=i;j<nums.length-1;j++){
+      nums[j]=nums[j+1];
+      }
+      nums[nums.length-1]=0;
+      }
+      console.log(nums)
+  }
+  return nums;
+
+};
+console.log(moveZeroes([0,0,1]))
